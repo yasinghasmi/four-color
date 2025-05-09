3ea1250f2e636941e1d6a9841304d4a32ff3724a
Yasin Ghasemi <86905546+yasinghasmi@users.noreply.github.com>
2025-05-09 17:12:49 +0200

    Revert "fix test 6 (#47)"
    This reverts commit 54eb0c3825e2f5d6251f69aa263c4bade9f16283.

--- src/components/GraphVisualization.js
+++ src/components/GraphVisualization.js
@@ -5,7 +5,7 @@ const GraphVisualization = ({ graphData }) => {
   const graphRef = useRef(null);
 
   // utilizing useeffect for when the graph data is getting updated
-  // test6
+  // test5
   useEffect(() => {
     if (!graphData) return;
 
