d856cbbd7cefd49c939010e561bdf117cd09bad9
YASINZEN\yasin <yasinghasemi.2003@gmail.com>
2025-05-09 11:30:23 +0200

    Update comments in GraphVisualization component for clarity

--- src/components/GraphVisualization.js
+++ src/components/GraphVisualization.js
@@ -5,7 +5,8 @@ const GraphVisualization = ({ graphData }) => {
   const graphRef = useRef(null);
 
   // utilizing useeffect for when the graph data is getting updated
-  // test
+  // test4
+  //test5
   useEffect(() => {
     if (!graphData) return;
 
