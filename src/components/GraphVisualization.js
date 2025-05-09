8431553435be67700e9879ef451df169a580f7ef
Yasin Ghasemi <86905546+yasinghasmi@users.noreply.github.com>
2025-05-09 16:42:12 +0200

    Revert "fix: test3 (#38)" (#39)
    This reverts commit 8dc16953d477df17ce9e53c04f5b0b4e08e523f1.

--- src/components/GraphVisualization.js
+++ src/components/GraphVisualization.js
@@ -5,7 +5,7 @@ const GraphVisualization = ({ graphData }) => {
   const graphRef = useRef(null);
 
   // utilizing useeffect for when the graph data is getting updated
-  // test3
+  // test2
   useEffect(() => {
     if (!graphData) return;
 
