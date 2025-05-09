edb3537e787bae72ec536b3716e687602967b294
YASINZEN\yasin <yasinghasemi.2003@gmail.com>
2025-05-09 12:59:03 +0200

    feat(EC-12343): test 1

--- src/components/GraphVisualization.js
+++ src/components/GraphVisualization.js
@@ -5,6 +5,7 @@ const GraphVisualization = ({ graphData }) => {
   const graphRef = useRef(null);
 
   // utilizing useeffect for when the graph data is getting updated
+  // test 1
   useEffect(() => {
     if (!graphData) return;
 
