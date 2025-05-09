8ff4feb6420c95da52a44cb6c54a203bae92600b
YASINZEN\yasin <yasinghasemi.2003@gmail.com>
2025-05-09 12:57:11 +0200

    fix: remove unnecessary test comments in GraphVisualization component

--- src/components/GraphVisualization.js
+++ src/components/GraphVisualization.js
@@ -5,14 +5,6 @@ const GraphVisualization = ({ graphData }) => {
   const graphRef = useRef(null);
 
   // utilizing useeffect for when the graph data is getting updated
-  // test4
-  //test5
-  //test 6
-  //test 7
-  //test 8
-  //test 9
-  //test 10
-  //test 11
   useEffect(() => {
     if (!graphData) return;
 
