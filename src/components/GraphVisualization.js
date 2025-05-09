60bc4a600113eb77fbc4e67e5471ec990c29c783
YASINZEN\yasin <yasinghasemi.2003@gmail.com>
2025-05-09 15:11:33 +0200

    test(23534): test5

--- src/components/GraphVisualization.js
+++ src/components/GraphVisualization.js
@@ -5,7 +5,7 @@ const GraphVisualization = ({ graphData }) => {
   const graphRef = useRef(null);
 
   // utilizing useeffect for when the graph data is getting updated
-  // test 4
+  // test 5
   useEffect(() => {
     if (!graphData) return;
 
