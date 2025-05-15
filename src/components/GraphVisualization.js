dcb3b5797afd5445ace73dc2274045aad09c24b9
Yasin Ghasemi <86905546+yasinghasmi@users.noreply.github.com>
2025-05-15 12:08:14 +0200

    Revert "fix(Ec-235333): fix (#115)" (#117)
    This reverts commit 42c534c6eca9db0f9ff4ecd4c7713f459925873a.

--- src/components/GraphVisualization.js
+++ src/components/GraphVisualization.js
@@ -5,7 +5,7 @@ const GraphVisualization = ({ graphData }) => {
   const graphRef = useRef(null);
 
   // utilizing useeffect for when the graph data is getting updated
-  // test21
+  // test20
   useEffect(() => {
     if (!graphData) return;
 
