5e675f8f7be46e2eedbb7dc15fb7d136a8c00d51
Yasin Ghasemi <86905546+yasinghasmi@users.noreply.github.com>
2025-05-17 00:16:01 +0200

    Revert "Item77 (#327)" (#329)
    This reverts commit 822a00eec854dcaab3935de547f93beff9ba2a7a.

--- src/components/GraphVisualization.js
+++ src/components/GraphVisualization.js
@@ -5,13 +5,13 @@ const GraphVisualization = ({ graphData }) => {
   const graphRef = useRef(null);
 
   // utilizing useeffect for when the graph data is getting updated
-  // test77
+  // test76
   useEffect(() => {
     if (!graphData) return;
 
     const { edges, colors, maxColorsUsed } = graphData;
 
-    // Intitally,, wwhhen I implemented this logic I wanted to have automatic color generation, without need
+    // Intitally, wwhhen I implemented this logic I wanted to have automatic color generation, without need
     // to only limit it to 45 colors (which is max of our case).
     const generateColors = (numColors) => {
       const palette = [];
