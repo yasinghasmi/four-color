8072d99dbbc9b66677903ccc43ff8789302c5d02
YASINZEN\yasin <yasinghasemi.2003@gmail.com>
2025-05-16 16:57:48 +0200

    Revert "fix 43"
    This reverts commit 3add47da1776ca965144649f1c3240c9b4702fd6.

--- src/components/GraphVisualization.js
+++ src/components/GraphVisualization.js
@@ -5,7 +5,7 @@ const GraphVisualization = ({ graphData }) => {
   const graphRef = useRef(null);
 
   // utilizing useeffect for when the graph data is getting updated
-  // test43
+  // test42
   useEffect(() => {
     if (!graphData) return;
 
