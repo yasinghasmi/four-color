ba577ad5e2f3399f045133d66150c0a5bc20915b
YASINZEN\yasin <yasinghasemi.2003@gmail.com>
2025-05-17 22:14:48 +0200

    fix(EC-92929):2

--- src/components/GraphVisualization.js
+++ src/components/GraphVisualization.js
@@ -5,7 +5,7 @@ const GraphVisualization = ({ graphData }) => {
   const graphRef = useRef(null);
 
   // utilizing useeffect for when the graph data is getting updated
-  // test90
+  // test92
   useEffect(() => {
     if (!graphData) return;
 
