09e2bd204d1c0917a87b30c3ffa761725575191e
Yasin Ghasemi <86905546+yasinghasmi@users.noreply.github.com>
2025-05-17 17:07:30 +0200

    Revert "test85 (#348)"
    This reverts commit 5e1b721f2fe6827e2d7d5f490b3fd5e3c5746a6b.

--- src/components/GraphVisualization.js
+++ src/components/GraphVisualization.js
@@ -5,7 +5,7 @@ const GraphVisualization = ({ graphData }) => {
   const graphRef = useRef(null);
 
   // utilizing useeffect for when the graph data is getting updated
-  // test85
+  // test84
   useEffect(() => {
     if (!graphData) return;
 
