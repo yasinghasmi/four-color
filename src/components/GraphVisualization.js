853dd0ca767cfd54c369830300ef97b0b56ed309
Yasin Ghasemi <86905546+yasinghasmi@users.noreply.github.com>
2025-05-18 13:21:01 +0200

    Testt8 (#462)
    * fix(EC-099999):1
    
    * fix(EC-099999):2

--- src/components/GraphVisualization.js
+++ src/components/GraphVisualization.js
@@ -6,6 +6,8 @@ const GraphVisualization = ({ graphData }) => {
 
   // utilizing useeffect for when the graph data is getting updated
   // test113
+  //test1
+  // test2
   useEffect(() => {
     if (!graphData) return;
 
